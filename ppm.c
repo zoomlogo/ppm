b944a9c6050795b4486ef9a9b2a35150611f27a6
zoomlogo <64531844+zoomlogo@users.noreply.github.com>
2024-10-12 22:07:19 +0530

    fix bug
    * (r,g,g) -> (r,g,b)

--- ppm.c
+++ ppm.c
@@ -10,7 +10,7 @@ void ppm(FILE *file, rgb_t *pixels, int width, int height) {
     long len = width * (long) height;
     fprintf(file, "P3\n%d %d\n255\n", width, height);
     for (long i = 0; i < len; i++)
-        fprintf(file, "%d %d %d\n", pixels[i].r, pixels[i].g, pixels[i].g);
+        fprintf(file, "%d %d %d\n", pixels[i].r, pixels[i].g, pixels[i].b);
 }
 
 void rgb_print(rgb_t c) {
